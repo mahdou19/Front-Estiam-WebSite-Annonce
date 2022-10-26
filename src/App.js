c274e7fa353cb37188488f917d6400e521449948
Mamadou <mamadou.sall@42c.fr>
2022-10-26 17:30:46 +0200

    config Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Navbar from './components/Navbar/Navbar';
+import Register from './components/register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Worl</h1>
+    <div className='App'>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
