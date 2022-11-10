6c7bb4d3c9a6b3fd6579f626a508078e272a7abd
Mamadou <mamadou.sall@42c.fr>
2022-11-10 16:55:48 +0100

    fix registration form

--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -1,136 +1,97 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const onChangeInput = (e) => {};
 
-  const formData = new FormData()
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-        firstname: '',
-        lastname:'',
-        phone: '',
-        address: '',
-        zip_code: '',
-        city: '',
-        country:'',
-        identifical_file: {}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/registration');
 
-
-      });
-      const [selectedFile, setSelectedFile] = useState(null);
-    
-    
-      const onChangeInput = (e) => {
-        const { name, value } = e.target;
-        setUser({ ...user, [name]: value });
-      };
-    
-      const handleSubmit = async e => {
-        e.preventDefault()
-        try{
-            await axios.post('/registration', {...user})
-    
-            localStorage.setItem('FirstLogin', true)
-    
-            window.location.href = "/"
-    
-        }catch(err){
-            alert(err.response.data.msg)
-        }
-      }
-      return (
-        <div className='login-page'>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              name='firstname'
-              required
-              placeholder='Prénom'
-              value={user.firstname}
-              onChange={onChangeInput}
-            />
-             <input
-              type='text'
-              name='lastname'
-              required
-              placeholder='Nom'
-              value={user.lastname}
-              onChange={onChangeInput}
-            />
-
-            <input
-              type='email'
-              name='email'
-              required
-              placeholder='Email'
-              value={user.email}
-              onChange={onChangeInput}
-            />
-            <input
-              type='password'
-              name='password'
-              required
-              placeholder='Mot de passe'
-              value={user.password}
-              onChange={onChangeInput}
-            />
-              <input
-              type='number'
-              name='phone'
-              required
-              placeholder='phone(Ex:0700000000'
-              value={user.phone}
-              onChange={onChangeInput}
-            />
-            <input
-              type='text'
-              name='address'
-              required
-              placeholder='Adresse'
-              value={user.address}
-              onChange={onChangeInput}
-            />
-            <input
-              type='text'
-              name='zip_code'
-              required
-              placeholder='Code Postal'
-              value={user.zip_code}
-              onChange={onChangeInput}
-            />
-            <input
-              type='text'
-              name='city'
-              required
-              placeholder='Ville'
-              value={user.city}
-              onChange={onChangeInput}
-            />
-             <input
-              type='text'
-              name='country'
-              required
-              placeholder='Pays'
-              value={user.country}
-              onChange={onChangeInput}
-            />
-              <input
-          type="file"
-          name='identifical_file'
-          value={user.identifical_file}
+      window.location.href = '/';
+    } catch (err) {
+      alert(err.response.data.msg);
+    }
+  };
+  return (
+    <div className='login-page'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='firstname'
+          required
+          placeholder='Prénom'
+          onChange={onChangeInput}
+        />
+        <input
+          type='text'
+          name='lastname'
+          required
+          placeholder='Nom'
           onChange={onChangeInput}
         />
 
-            <div>
-              <button type='submit'>S'inscrire</button>
-              <Link to='login'>Se Connecter</Link>
-            </div>
-          </form>
-        </div>
-      );
-}
+        <input
+          type='email'
+          name='email'
+          required
+          placeholder='Email'
+          onChange={onChangeInput}
+        />
+        <input
+          type='password'
+          name='password'
+          required
+          placeholder='Mot de passe'
+          onChange={onChangeInput}
+        />
+        <input
+          type='number'
+          name='phone'
+          required
+          placeholder='phone(Ex:0700000000'
+          onChange={onChangeInput}
+        />
+        <input
+          type='text'
+          name='address'
+          required
+          placeholder='Adresse'
+          onChange={onChangeInput}
+        />
+        <input
+          type='text'
+          name='zip_code'
+          required
+          placeholder='Code Postal'
+          onChange={onChangeInput}
+        />
+        <input
+          type='text'
+          name='city'
+          required
+          placeholder='Ville'
+          onChange={onChangeInput}
+        />
+        <input
+          type='text'
+          name='country'
+          required
+          placeholder='Pays'
+          onChange={onChangeInput}
+        />
+        <input type='file' name='identifical_file' onChange={onChangeInput} />
 
-export default Register
+        <div>
+          <button type='submit'>S'inscrire</button>
+          <Link to='login'>Se Connecter</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
